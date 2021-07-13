b33b8562a790307655f93cbe6dff63188607a185
JakeFC <jacobchavera@yahoo.com>
2021-07-13 22:34:09 +0000

    changed to lowercase hex format

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -8,7 +8,7 @@ namespace _4_print_hexa
         {
             int i;
             for (i = 0; i <= 98; i++)
-                Console.WriteLine("{0} = 0x{1}", i, i.ToString("X"));
+                Console.WriteLine("{0} = 0x{1}", i, i.ToString("x"));
         }
     }
 }
