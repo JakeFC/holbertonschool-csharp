a0ce35b6649fd697b30fd7ca2fe851d9c099ad6b
JakeFC <jacobchavera@yahoo.com>
2021-07-13 22:48:48 +0000

    task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string sep = ", ";
+            for (i = 0; i < 99; i++)
+            {
+                Console.Write("{0}{1}", i.ToString(), sep);
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
