dd9a0870f4bb68a461e955983e2f988fc76753b2
JakeFC <jacobchavera@yahoo.com>
2021-07-13 22:26:34 +0000

    task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabet/3-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            for (c = 'a'; c <= 'z'; c++)
+                if (c != 'q' && c != 'e')
+                    Console.Write(c);
+        }
+    }
+}
