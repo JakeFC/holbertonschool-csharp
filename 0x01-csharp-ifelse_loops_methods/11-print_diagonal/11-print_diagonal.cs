62f37ce619ea920dd2ce4970b2491dd74beb096d
JakeFC <jacobchavera@yahoo.com>
2021-07-13 23:19:21 +0000

    task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; length > 0; length--, i++)
+        {
+            for (int j = i; j > 0; j--)
+                Console.Write(' ');
+            Console.WriteLine('\\');
+        }
+        Console.WriteLine("");
+    }
+}
