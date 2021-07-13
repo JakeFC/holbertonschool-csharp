5c618ff31c56e9c02310c8681c7ce5e5965b50c0
JakeFC <jacobchavera@yahoo.com>
2021-07-13 22:32:24 +0000

    task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i <= 98; i++)
+                Console.WriteLine("{0} = 0x{1}", i, i.ToString("X"));
+        }
+    }
+}
