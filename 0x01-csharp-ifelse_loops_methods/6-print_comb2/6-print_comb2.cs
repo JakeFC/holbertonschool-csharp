933e7499d410ce49492e307ac2b9f3af292666f6
JakeFC <jacobchavera@yahoo.com>
2021-07-13 22:53:06 +0000

    task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string sep = ", ";
+            for (i = 0; i < 89; i++)
+            {
+                if (i % 10 > i / 10)
+                    Console.Write("{0}{1}", i.ToString("D2"), sep);
+            }
+            Console.WriteLine(i.ToString("D2"));
+        }
+    }
+}
