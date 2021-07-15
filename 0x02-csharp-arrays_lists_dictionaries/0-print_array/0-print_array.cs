f0b2e65e1752bd1f4fe8ba75528c2b46531ae902
JakeFC <jacobchavera@yahoo.com>
2021-07-15 00:32:38 +0000

    task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] arr = new int[size];
+        string sep = " ";
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = i;
+            if (i == size - 1)
+                sep = "";
+            Console.Write(i.ToString() + sep);
+        }
+        Console.WriteLine();
+        return arr;
+    }
+}
