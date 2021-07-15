94634b408d51c522548edacf470fe93c86ae5278
JakeFC <jacobchavera@yahoo.com>
2021-07-15 01:25:56 +0000

    task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        var isEven = new List<bool>();
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+                isEven.Add(true);
+            else
+                isEven.Add(false);
+        }
+        return isEven;
+    }
+}
