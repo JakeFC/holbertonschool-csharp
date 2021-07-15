8b2c5a8f1e546a6a320e970d77ce3e46c7a35597
JakeFC <jacobchavera@yahoo.com>
2021-07-15 03:17:52 +0000

    task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int y = 0; y < myMatrix.GetLength(0); y++)
+        {
+            for (int x = 0; x < myMatrix.GetLength(1); x++)
+            {
+                myMatrix[y, x] *= myMatrix[y, x];
+            }
+        }
+        return myMatrix;
+    }
+}
