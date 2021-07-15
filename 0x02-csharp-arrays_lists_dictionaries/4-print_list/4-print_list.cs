abc4179f43cc79037e00a01d062b4caf55fc5cf8
JakeFC <jacobchavera@yahoo.com>
2021-07-15 01:12:09 +0000

    task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        var lst = new List<int>();
+        int i = 0;
+        for (; i < size - 1; i++)
+        {
+            lst.Add(i);
+            Console.Write(i.ToString() + " ");
+        }
+        if (size > 0)
+        {
+            lst.Add(i);
+            Console.Write(i);
+        }
+        Console.WriteLine();
+        return lst;
+    }
+}
