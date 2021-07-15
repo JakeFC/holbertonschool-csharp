53786601b4fd95658ee1829656bbdfffd7126b1c
JakeFC <jacobchavera@yahoo.com>
2021-07-15 00:55:44 +0000

    task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        int i = array.Length - 1;
+        string sep = " ";
+        for (; i >= 0; i--)
+        {
+            if (i == 0)
+                sep = "";
+            Console.Write(array[i].ToString() + sep);
+        }
+        Console.WriteLine();
+    }
+}
