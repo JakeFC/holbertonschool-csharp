7bd8e799cd43506a066947b28926dcab3400eec9
JakeFC <jacobchavera@yahoo.com>
2021-07-15 02:16:58 +0000

    task 11

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var newDict = new Dictionary<string, int>();
+        foreach (string key in myDict.Keys)
+            newDict[key] = myDict[key] * 2;
+        return newDict;
+    }
+}
