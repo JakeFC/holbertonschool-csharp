f4cd92bce4bbddf78f7d3eee60b83dce824c03a4
JakeFC <jacobchavera@yahoo.com>
2021-07-15 03:10:16 +0000

    task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[, ] arr = new int[5, 5];
+        arr[2, 2] = 1;
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Console.Write(arr[y, x].ToString());
+                if (x < 4)
+                    Console.Write(" ");
+                else
+                    Console.WriteLine();
+            }
+        }
+    }
+}
