4b0f0e1679e931f3a7ac41d555375ac5bca8e591
JakeFC <jacobchavera@yahoo.com>
2021-07-16 01:10:15 +0000

    task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        // returns a sorted list of all elements present in one or the other list but not both
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            var newList = new List<int> {};
+            foreach (int a in list1)
+            {
+                if (!list2.Contains(a))
+                    newList.Add(a);
+            }
+            foreach (int b in list2)
+            {
+                if (!list1.Contains(b))
+                    newList.Add(b);
+            }
+            newList.Sort();
+            return newList;
+        }
+    }
