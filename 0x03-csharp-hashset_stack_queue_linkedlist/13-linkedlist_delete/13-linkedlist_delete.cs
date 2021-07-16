7225dad4dde1ee23c2484a96d7d61ed1f96f16e4
JakeFC <jacobchavera@yahoo.com>
2021-07-16 03:03:54 +0000

    task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Deletes the node at given position in a LinkedList.
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+        foreach (int x in myLList)
+        {
+            if (i == index)
+            {
+                myLList.Remove(x);
+                break;
+            }
+            i++;
+        }
+    }
+}
