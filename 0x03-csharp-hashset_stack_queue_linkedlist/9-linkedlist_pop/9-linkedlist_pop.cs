9aeadc9acf440dacba75380ac8d488a2b7990580
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:32:41 +0000

    task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Deletes the head node of a LinkedList and returns that node’s data.
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.First == null)
+            return 0;
+        return myLList.First.Value;
+    }
+}
