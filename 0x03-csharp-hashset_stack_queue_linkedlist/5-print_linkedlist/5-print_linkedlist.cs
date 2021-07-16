35b157855e9d11c36ade6b1c4bc56ffa3322d161
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:15:06 +0000

    task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Creates and prints a LinkedList of integers of a given size.
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        var linkly = new LinkedList<int>{};
+        for (int i = 0; i < size; i++)
+            {
+                linkly.AddLast(i);
+                Console.WriteLine(i);
+            }
+        return linkly;
+    }
+}
