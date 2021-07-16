ae6da28011d18a4011435fb75c95e15600fc0c24
JakeFC <jacobchavera@yahoo.com>
2021-07-16 00:56:58 +0000

    task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // adds all unique integers in a list
+    public static int Sum(List<int> myList)
+    {
+        var set = new HashSet<int>(myList);
+        int total = 0;
+        foreach (int x in set)
+            total += x;
+        return total;
+    }
+}
