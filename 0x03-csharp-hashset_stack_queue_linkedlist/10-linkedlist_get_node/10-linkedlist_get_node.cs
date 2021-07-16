d6d1cc879a3c7e2e519fd596f08f0c749dd3e392
JakeFC <jacobchavera@yahoo.com>
2021-07-16 15:45:34 +0000

    edited with one-line solution

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class LList
 {
     // Returns the value of the nth node of a LinkedList.
     public static int GetNode(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> current = myLList.First;
-        for (int i = 0; current != null; current = current.Next, i++)
-            {
-                if (i == n)
-                    return current.Value;
-            }
-        return 0;
+        //LinkedListNode<int> current = myLList.First;
+        //for (int i = 0; current != null; current = current.Next, i++)
+        //    {
+        //        if (i == n)
+        //            return current.Value;
+        //    }
+        //return 0;
+        return myLList.ElementAtOrDefault(n);
     }
 }
