70d604d9e492a035523499cac24f35a9f5250625
JakeFC <jacobchavera@yahoo.com>
2021-07-16 01:27:55 +0000

    added the rest of the instructions

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,7 +5,7 @@ class MyStack
 {
     // Prints the number of items in aStack, the item at the top of aStack without removing it,
     // and if aStack contains a given item search. If aStack contains the given item search,
-    // remove all items up to and including search. Return the stack.
+    // remove all items up to and including search. Return the stack. Add a new given item newItem to aStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
@@ -15,8 +15,10 @@ class MyStack
         {
             while (aStack.Peek() != search)
                 aStack.Pop();
+            aStack.Pop();
         }
         Console.WriteLine("Stack contains \"{0}\": {1}", search, contains);
+        aStack.Push(newItem);
         return aStack;
     }
 }
