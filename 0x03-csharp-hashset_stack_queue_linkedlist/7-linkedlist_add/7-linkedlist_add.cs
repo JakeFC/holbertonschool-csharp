bdfb594414ba02bfaeb70882f3dab7477fec4bbb
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:21:44 +0000

    task 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Adds a node to the beginning of a LinkedList.
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+        return myLList.First;
+    }
+}
