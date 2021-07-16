b3813c0520d5b3fcfb0ed1e795e671748d2a7093
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:40:24 +0000

    task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Returns the value of the nth node of a LinkedList.
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; current != null; current = current.Next, i++)
+            {
+                if (i == n)
+                    return current.Value;
+            }
+        return 0;
+    }
+}
