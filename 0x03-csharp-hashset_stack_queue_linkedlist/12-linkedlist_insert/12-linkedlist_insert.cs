880ee6f0924d062a2843c69ca701c5b876a14213
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:50:19 +0000

    task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Inserts a new node in the correct position in an ordered LinkedList and returns it.
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        foreach (int x in myLList)
+            {
+                if (n < x)
+                    break;
+                current = current.Next;
+            }
+        myLList.AddBefore(current, n);
+        return myLList.Find(n);
+    }
+}
