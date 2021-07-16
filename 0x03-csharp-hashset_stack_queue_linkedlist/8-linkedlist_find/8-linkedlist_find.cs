a60cdebe6ed1c79677f5ffa2287e554736a0561c
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:27:51 +0000

    task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Finds a value in a LinkedList and returns its index position in the list.
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        foreach (int x in myLList)
+        {
+            if (x == value)
+                return i;
+            i++;
+        }
+        return -1;
+    }
+}
