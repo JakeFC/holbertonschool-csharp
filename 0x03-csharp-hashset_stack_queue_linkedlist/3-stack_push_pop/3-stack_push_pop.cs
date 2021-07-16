bd657f39894d04e764ee0d2c0f65b3a521f3bd39
JakeFC <jacobchavera@yahoo.com>
2021-07-16 01:25:38 +0000

    task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    // Prints the number of items in aStack, the item at the top of aStack without removing it,
+    // and if aStack contains a given item search. If aStack contains the given item search,
+    // remove all items up to and including search. Return the stack.
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        Console.WriteLine("Top item: {0}", aStack.Peek());
+        bool contains = aStack.Contains(search);
+        if (contains)
+        {
+            while (aStack.Peek() != search)
+                aStack.Pop();
+        }
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, contains);
+        return aStack;
+    }
+}
