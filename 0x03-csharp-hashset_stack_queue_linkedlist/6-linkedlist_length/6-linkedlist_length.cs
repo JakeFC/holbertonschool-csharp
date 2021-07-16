8c3a0f303297d62deb49071b807b5d267e4d61de
JakeFC <jacobchavera@yahoo.com>
2021-07-16 02:19:26 +0000

    task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Returns the number of elements in a LinkedList.
+    public static int Length(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int count = 0;
+        while (current != null)
+            {
+                current = current.Next;
+                count++;
+            }
+        return count;
+    }
+}
