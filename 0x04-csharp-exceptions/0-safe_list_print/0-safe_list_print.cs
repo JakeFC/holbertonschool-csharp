0127a3a5f457fe1e5ea47e1a6ea6d78793cb831c
JakeFC <jacobchavera@yahoo.com>
2021-07-19 16:55:28 +0000

    task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // Prints n elements of a list, if they exist, and returns the number
+    // of elements printed
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        for (; i < n; i++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return i;
+            }
+        }
+        return i;
+    }
+}
