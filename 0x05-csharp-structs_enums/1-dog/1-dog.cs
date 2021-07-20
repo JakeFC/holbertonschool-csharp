30502c98818a67a282c1dbbf70ac6b3e4ae83237
JakeFC <jacobchavera@yahoo.com>
2021-07-20 19:49:08 +0000

    task 1

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// A newly defined enum Rating with the values Good, Great, Excellent.
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+// A struct Dog with 4 members of various types.
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
