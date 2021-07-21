9dd56016f81a06da61d388bbd4a7afbd5275d506
JakeFC <jacobchavera@yahoo.com>
2021-07-21 22:37:15 +0000

    task 5

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Class with members health and name.</summary>
+    public class Zombie
+    {
+        int health;
+        string name = "(No name)";
+        ///<summary>(String)Name of Zombie object.</summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        /// <summary>Creates a Zombie with health set to 0.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary>Creates a Zombie and sets health.</summary>
+        /// <param name="value">The Zombie health.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+        /// <summary>Returns Zombie health(int) value.</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+        /// <summary>Returns the formatted string "Zombie Name: name / Total Health: health".</summary>
+        public override string ToString()
+        {
+            return String.Format("Zombie Name: {0} / Total Health: {1}", this.name, this.health);
+        }
+    }
+}
