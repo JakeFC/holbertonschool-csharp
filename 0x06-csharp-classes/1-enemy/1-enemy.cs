19088aa6d0206c1bb07611a9e947d42fd852d365
JakeFC <jacobchavera@yahoo.com>
2021-07-21 21:52:13 +0000

    task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Class with member health.</summary>
+    public class Zombie
+    {
+        /// <summary>Int health of Zombie.</summary>
+        public int health;
+        
+        /// <summary>Creates a Zombie with health set to 0.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
