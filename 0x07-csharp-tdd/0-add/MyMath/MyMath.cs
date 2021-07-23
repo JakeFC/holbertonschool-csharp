5e77264824991e9384c30d42c56e6f4b3104fbba
JakeFC <jacobchavera@yahoo.com>
2021-07-23 20:24:45 +0000

    added documentation for parameters

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -6,6 +6,8 @@ namespace MyMath
     public class Operations
     {
         /// <summary>Returns the sum of two integers.</summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
         public static int Add(int a, int b)
         {
             return a + b;
