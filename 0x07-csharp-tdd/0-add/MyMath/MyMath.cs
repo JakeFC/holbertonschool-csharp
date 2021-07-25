5fb25bee167bfe51d9ea9224b3802939534f13ee
JakeFC <jacobchavera@yahoo.com>
2021-07-25 23:22:57 +0000

    extended xml documentation for Add method

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -5,9 +5,10 @@ namespace MyMath
     /// <summary>MyMath class containing methods for math operations.</summary>
     public class Operations
     {
-        /// <summary>Returns the sum of two integers.</summary>
+        /// <summary>Adds two integers.</summary>
         /// <param name="a">First integer.</param>
         /// <param name="b">Second integer.</param>
+        /// <returns>The sum of two integers.</returns>
         public static int Add(int a, int b)
         {
             return a + b;
