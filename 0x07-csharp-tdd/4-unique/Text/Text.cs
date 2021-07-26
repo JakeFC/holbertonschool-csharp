6e2ebff2d8eee3d1c3846396626e6c05c8af861a
JakeFC <jacobchavera@yahoo.com>
2021-07-26 17:59:02 +0000

    simplified logic to use Split for finding character count

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -13,9 +13,13 @@ namespace Text
             for (int i = 0; i < s.Length; i++)
             {
                 char c = s[i];
-                int lengthRemaining = s.Length - i - 1;
-                if (!(s.Substring(0, i).Contains(c.ToString())) &&
-                    (lengthRemaining == 0 || !(s.Substring(i + 1, lengthRemaining).Contains(c.ToString()))))
+            //    int lengthRemaining = s.Length - i - 1;
+            //    if (!(s.Substring(0, i).Contains(c.ToString())) &&
+            //        (lengthRemaining == 0 || !(s.Substring(i + 1, lengthRemaining).Contains(c.ToString()))))
+            //    {
+            //        return i;
+            //    }
+                if (s.Split(c).Length - 1 == 1)
                 {
                     return i;
                 }
