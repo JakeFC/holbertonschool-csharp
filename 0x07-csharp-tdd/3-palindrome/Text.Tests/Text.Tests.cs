30d90959187617b7c7cf4fcdc4b51c6c13e51c33
JakeFC <jacobchavera@yahoo.com>
2021-07-26 16:24:00 +0000

    task 3

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    /// <summary>Unittests for Str method functionality.</summary>
+    public class StrTests
+    {
+        /// <summary>Tests Str.IsPalindrome() with a basic palindrome of lowercase letters.</summary>
+        [Test]
+        public void BasicPalindrome()
+        {
+            Assert.True(Str.IsPalindrome("level"));
+        }
+
+        /// <summary>Tests Str.IsPalindrome() with a non-palindrome of letters.</summary>
+        [Test]
+        public void NotPalindrome()
+        {
+            Assert.False(Str.IsPalindrome("tacos"));
+        }
+
+        /// <summary>Tests Str.IsPalindrome() with a palindrome of upper and lowercase letters.</summary>
+        [Test]
+        public void IgnoreCasePalindrome()
+        {
+            Assert.True(Str.IsPalindrome("Racecar"));
+        }
+
+        /// <summary>Tests Str.IsPalindrome() with a palindrome of letters, spaces, and punctuation.</summary>
+        [Test]
+        public void IgnorePunctuationPalindrome()
+        {
+            Assert.True(Str.IsPalindrome("A man, a plan, a canal: Panama."));
+        }
+
+        /// <summary>Tests Str.IsPalindrome() with an empty string.</summary>
+        [Test]
+        public void EmptyPalindrome()
+        {
+            Assert.True(Str.IsPalindrome(""));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>Custom string class.</summary>
+    public class Str
+    {
+        /// <summary>Checks whether the letters of a string form a pallindrome.</summary>
+        /// <param name="s">String input.</param>
+        /// <returns>True if palindrome, or false if not.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            string removeChars = "";
+            foreach (char c in s)
+            {
+                if (c < 97 || c > 122)
+                    removeChars += c;
+            }
+            s = String.Join("", s.Split(removeChars.ToCharArray()));
+            int start = 0;
+            for (int end = s.Length - 1; start < end; start++, end--)
+            {
+                if (s[start] != s[end])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
