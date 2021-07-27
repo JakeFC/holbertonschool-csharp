28efe8acd6e495325b3da71a9e306ceb4b744a46
JakeFC <jacobchavera@yahoo.com>
2021-07-27 21:13:59 +0000

    fixed code block comment's indent

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -13,12 +13,12 @@ namespace Text
             for (int i = 0; i < s.Length; i++)
             {
                 char c = s[i];
-            //    int lengthRemaining = s.Length - i - 1;
-            //    if (!(s.Substring(0, i).Contains(c.ToString())) &&
-            //        (lengthRemaining == 0 || !(s.Substring(i + 1, lengthRemaining).Contains(c.ToString()))))
-            //    {
-            //        return i;
-            //    }
+                // int lengthRemaining = s.Length - i - 1;
+                // if (!(s.Substring(0, i).Contains(c.ToString())) &&
+                //     (lengthRemaining == 0 || !(s.Substring(i + 1, lengthRemaining).Contains(c.ToString()))))
+                // {
+                //     return i;
+                // }
                 if (s.Split(c).Length - 1 == 1)
                 {
                     return i;
