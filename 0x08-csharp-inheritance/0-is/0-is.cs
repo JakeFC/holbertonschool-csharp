0b05b497aacde2b976f83b99e6b23fd9b9e0fab6
JakeFC <jacobchavera@yahoo.com>
2021-07-27 22:07:02 +0000

    removed unnecessary brackets

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -8,9 +8,7 @@ class Obj
     public static bool IsOfTypeInt(object obj)
     {
         if (obj.GetType() == typeof(int))
-        {
             return true;
-        }
         return false;
     }
 }
