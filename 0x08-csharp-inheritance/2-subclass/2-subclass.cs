87c4dcb2c104eb4121d5e1129b69682d23e025ae
JakeFC <jacobchavera@yahoo.com>
2021-07-27 22:17:40 +0000

    task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Obj
+{
+    ///<summary>Checks whether derivedType is a subclass of baseType.</summary>
+    ///<param name="derivedType">Potential subclass to be checked.</param>
+    ///<param name="baseType">Parent class to be checked against.</param>
+    ///<returns>True if subclass, otherwise false.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
