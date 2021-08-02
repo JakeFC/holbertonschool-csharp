d97836616ac9e1c4b7e70542cd8313633442d2af
JakeFC <jacobchavera@yahoo.com>
2021-08-02 22:11:01 +0000

    removed extra bracket at end and tabbing

--- 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.Math;
 
-	///<summary>Defines vector functions.</summary>
-    class VectorMath
-    {
-		///<summary>Finds the length of a 2D or 3D vector.</summary>
-		///<param name="vector">Array coordinates of 2D or 3D vector.</param>
-		///<returns>The length of the vector, or -1.</returns>
-        public static double Magnitude(double[] vector)
-        {
-			double length;
-            if (vector.Length < 2 || vector.Length > 3)
-			{
-				return -1;
-			}
-			foreach (double element in vector)
-			{
-				length += element * element;
-			}
-			return Math.Round(Math.Sqrt(length), 2);
-        }
-    }
+///<summary>Defines vector functions.</summary>
+class VectorMath
+{
+	///<summary>Finds the length of a 2D or 3D vector.</summary>
+	///<param name="vector">Array coordinates of 2D or 3D vector.</param>
+	///<returns>The length of the vector, or -1.</returns>
+	public static double Magnitude(double[] vector)
+	{
+		double length;
+		if (vector.Length < 2 || vector.Length > 3)
+		{
+			return -1;
+		}
+		foreach (double element in vector)
+		{
+			length += element * element;
+		}
+		return Math.Round(Math.Sqrt(length), 2);
+	}
 }
