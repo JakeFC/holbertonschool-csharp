b24122efb58e1a92086beff218c3c56176665066
JakeFC <jacobchavera@yahoo.com>
2021-08-02 22:58:49 +0000

    task 9

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+///<summary>Defines vector functions.</summary>
+class VectorMath
+{
+	///<summary>Multiplies a vector and scalar.</summary>
+	///<param name="vector">Vector to be scaled.</param>
+	///<param name="scalar">Scalar to multiply by.</param>
+	///<returns>The new vector, or -1 if vector is not 2D or 3D.</returns>
+	public static double[] Multiply(double[] vector, double scalar)
+	{
+		if (vector.Length < 2 || vector.Length > 3)
+			return new double[]{-1};
+		var result = new double[vector.Length];
+		for (int i = 0; i < vector.Length; i++)
+		{
+			result[i] = vector[i] * scalar;
+		}
+		return result;
+	}
+}
