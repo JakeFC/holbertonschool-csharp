998299cd8d1cbc09fce49d0dbdb689f9d18e988c
JakeFC <jacobchavera@yahoo.com>
2021-08-03 22:41:41 +0000

    task 0

---
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+///<summary>Custom generic class of Queue with type T.</summary>
+public class Queue<T>
+{
+	///<summary>Returns the Queue's type.</summary>
+	public Type CheckType()
+	{
+		return this.GetType().GetGenericArguments()[0];
+	}
+}
