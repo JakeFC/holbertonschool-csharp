1f786d3b349f75fc2fc63a91d1d8ed47b69aa111
JakeFC <jacobchavera@yahoo.com>
2021-08-03 01:41:26 +0000

    task 18

---
+++ 0x00-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+///<summary>Defines functions for matrices.</summary>
+class MatrixMath
+{
+	///<summary>Multiplies two matrices.</summary>
+	///<param name="matrix1">First matrix input.</param>
+	///<param name="matrix2">Second matrix input.</param>
+	///<returns>The resulting matrix, or one containg -1 if not possible.</returns>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		int mOneY = matrix1.GetUpperBound(0);
+		int mOneX = matrix1.GetUpperBound(1);
+		int mTwoY = matrix2.GetUpperBound(0);
+		int mTwoX = matrix2.GetUpperBound(1);
+		if (mOneX != mTwoY)
+			return new double[,]{{-1},};
+		var result = new double[mOneY, mTwoX];
+		for (int y = 0; y <= mOneY; y++)
+		{
+			for (int x = 0; x <= mTwoX; x++)
+			{
+				double total = 0;
+				for (int i = 0; i <= mOneX; i++)
+				{
+					total += matrix1[y, i] * matrix2[i, x];
+				}
+				result [y, x] = total;
+			}
+		}
+		return result;
+	}
+}
