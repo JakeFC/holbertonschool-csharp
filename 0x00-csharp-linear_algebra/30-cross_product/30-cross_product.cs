00c7ae6f50ed78c6b16f60a794f200e781af55fd
JakeFC <jacobchavera@yahoo.com>
2021-08-03 04:25:29 +0000

    fixed order of determinant subtraction for index 1 of result

--- 0x00-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x00-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -13,7 +13,7 @@ class VectorMath
 			return new double[]{-1};
 		var result = new double[3];
 		result[0] = Math.Round((vector1[1] * vector2[2] - vector1[2] * vector2[1]), 2);
-		result[1] = Math.Round((vector1[0] * vector2[2] - vector1[2] * vector2[0]), 2);
+		result[1] = Math.Round((vector1[2] * vector2[0] - vector1[0] * vector2[2]), 2);
 		result[2] = Math.Round((vector1[0] * vector2[1] - vector1[1] * vector2[0]), 2);
 		return result;
 	}
