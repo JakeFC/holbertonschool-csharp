78e7d31cc76c1a6b2dd873bed55630026b259c24
JakeFC <jacobchavera@yahoo.com>
2021-08-03 00:55:01 +0000

    task 16

---
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+///<summary>Defines functions for matrices.</summary>
+class MatrixMath
+{
+	///<summary>Multiplies a matrix and scalar.</summary>
+	///<param name="matrix">Matrix to be multiplied.</param>
+	///<param name="scalar">Number to multiply by.</param>
+	///<returns>The new matrix, or -1 if not 2D or 3D matrix.</returns>
+	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+	{
+		int upper0 = matrix.GetUpperBound(0);
+		int upper1 = matrix.GetUpperBound(1);
+		if (upper0 < 2 || upper0 > 3 || upper0 != upper1)
+			return new double[,]{{-1},};
+		for (int i = 0; i <= matrix.GetUpperBound(0); i++)
+		{
+			for (int j = 0; j <= matrix.GetUpperBound(1); j++)
+			{
+				matrix[i, j] *= scalar;
+			}
+		}
+		return matrix;
+	}
+}
