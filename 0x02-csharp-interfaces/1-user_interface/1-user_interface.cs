8f11eaceeb072a7f88649bbe816c14a6f65aac54
JakeFC <jacobchavera@yahoo.com>
2021-08-04 23:56:00 +0000

    task 1

---
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+///<summary>Abstract class which defines a name and ToString() method.</summary>
+public abstract class Base
+{
+	///<summary>Name of the object.</summary>
+	public string name { get; set; }
+
+	///<summary>Returns a string representation of the object.</summary>
+	public override string ToString()
+	{
+		return String.Format("{0} is a {1}", name, this.GetType());
+	}
+}
+
+///<summary>Contract specifying interaction.</summary>
+public interface IInteractive
+{
+	///<summary>Defines interaction.</summary>
+	void Interact();
+}
+
+///<summary>Contract specifying durability.</summary>
+public interface IBreakable
+{
+	///<summary>Durability of object.</summary>
+	int durability { get; set; }
+
+	///<summary>Defines durability break.</summary>
+	void Break();
+}
+
+///<summary>Contract specifying collectability.</summary>
+public interface ICollectible
+{
+	///<summary>Collected status of object.</summary>
+	bool isCollected { get; set; }
+
+	///<summary>Defines collection.</summary>
+	void Collect();
+}
+
+///<summary>Child class of Base implementing IInteractive, IBreakable, and ICollectible interfaces.</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectible
+{
+	///<summary>Durability of object.</summary>
+	public int durability { get; set; }
+	///<summary>Collected status of object.</summary>
+	public bool isCollected { get; set; }
+
+	///<summary>Defines interaction.</summary>
+	public void Interact(){}
+	///<summary>Defines durability break.</summary>
+	public void Break(){}
+	///<summary>Defines collection.</summary>
+	public void Collect(){}
+}
