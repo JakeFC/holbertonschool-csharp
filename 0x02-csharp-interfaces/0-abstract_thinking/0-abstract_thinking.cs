d7a0f5051ebaa647e85f42a17dbbbb85e8413a9d
JakeFC <jacobchavera@yahoo.com>
2021-08-04 23:41:06 +0000

    task 0

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Abstract class which defines a name and ToString() method.</summary>
+public abstract class Base
+{
+	///<summary>Name of the object.</summary>
+	public string name;
+
+	///<summary>Returns a string representation of the object.</summary>
+	public override string ToString()
+	{
+		return String.Format("{0} is a {1}", name, this.GetType());
+	}
+}
